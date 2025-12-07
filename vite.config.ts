7efb37490ed17f75c9979107dc3fe707a967bf3e
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-12-07 22:02:07 +0800

    feat!: switch to bun (#13)
    * feat: switch to bun
    
    * feat(ci): switch to bun
    
    * chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: "./",
   define: {
-    'process.env': {}
+    "process.env": {},
   },
-  resolve: {
-    alias: {
-      vue: 'https://esm.sh/vue@3.5.13'
-    }
-  }
-})
+});
