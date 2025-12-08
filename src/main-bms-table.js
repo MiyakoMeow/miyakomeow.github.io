9a337aebe037d0ef09899d3c0eeea5a4af6aef92
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-12-08 18:06:29 +0800

    fix: inline main.js (#19)
    * chore: update
    
    * fix: inline main.js

--- src/main-bms-table.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import "./styles/main.css";
-import BMSTable from "./pages/BMSTable.vue";
-
-const app = createApp(BMSTable);
-app.mount("#app");
--- src/main-bms.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import "./styles/main.css";
-import BMS from "./pages/BMS.vue";
-
-const app = createApp(BMS);
-app.mount("#app");
