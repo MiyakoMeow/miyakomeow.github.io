9800df968e51e08d912bed9abb3524e147501959
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-12-09 08:43:59 +0800

    feat: dep config, eslint, nesting (#36)
    * add: postcss-nesting
    
    * fix: use eslint.config.mjs, add tailwind lint
    
    * mv: postcss config to mjs
    
    * add: eslint css config
    
    * add: eslint postcss(tailwind) config
    
    * add: eslint vue scoped css config

---
+++ eslint.config.mjs
@@ -0,0 +1,91 @@
+import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginBetterTailwind from "eslint-plugin-better-tailwindcss";
+import globals from "globals";
+import css from "@eslint/css";
+import { tailwind4 } from "tailwind-csstree";
+import vueScopedCss from "eslint-plugin-vue-scoped-css";
+
+export default defineConfigWithVueTs(
+  vueTsConfigs.recommended,
+  {
+    files: ["**/*.vue"],
+    plugins: {
+      "vue-scoped-css": vueScopedCss,
+    },
+    rules: {
+      "vue-scoped-css/no-parsing-error": "error",
+      "vue-scoped-css/enforce-style-type": "off",
+      "vue-scoped-css/no-unused-selector": "off",
+      "vue-scoped-css/no-unused-keyframes": "off",
+    },
+  },
+  {
+    ignores: ["dist/**", "node_modules/**", "bun.lockb"],
+  },
+  {
+    files: ["**/*.vue"],
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  },
+  {
+    files: ["**/*.{vue,ts,js}"],
+    plugins: {
+      "better-tailwindcss": pluginBetterTailwind,
+    },
+    settings: {
+      "better-tailwindcss": {
+        entryPoint: "src/styles/main.pcss",
+      },
+    },
+    rules: {
+      "better-tailwindcss/enforce-consistent-class-order": "warn",
+      "better-tailwindcss/enforce-shorthand-classes": "warn",
+      "better-tailwindcss/enforce-consistent-line-wrapping": "off",
+      "better-tailwindcss/no-duplicate-classes": "warn",
+      "better-tailwindcss/no-unnecessary-whitespace": "warn",
+    },
+  },
+  {
+    files: ["**/*.css"],
+    plugins: {
+      css,
+    },
+    language: "css/css",
+    languageOptions: {
+      tolerant: true,
+    },
+    rules: {
+      "css/no-empty-blocks": "error",
+      "css/no-invalid-at-rules": "error",
+      "css/no-invalid-properties": "error",
+      "css/no-duplicate-imports": "error",
+      "css/use-baseline": "error",
+    },
+  },
+  {
+    files: ["**/*.pcss"],
+    plugins: {
+      css,
+    },
+    language: "css/css",
+    languageOptions: {
+      tolerant: true,
+      customSyntax: tailwind4,
+    },
+    rules: {
+      "css/no-empty-blocks": "error",
+      "css/no-invalid-at-rules": "off",
+      "css/no-invalid-properties": "off",
+      "css/no-duplicate-imports": "error",
+    },
+  },
+  {
+    files: ["vite.config.ts", "postcss.config.*"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  eslintConfigPrettier
+);
