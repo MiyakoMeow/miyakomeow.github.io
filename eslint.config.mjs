0a924ce31992d00b42f1a2abd531dfb49a00df17
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-12-09 08:32:14 +0800

    add: eslint postcss(tailwind) config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,11 @@
-import pluginVue from "eslint-plugin-vue";
 import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
 import eslintConfigPrettier from "eslint-config-prettier";
 import pluginBetterTailwind from "eslint-plugin-better-tailwindcss";
 import globals from "globals";
 import css from "@eslint/css";
+import { tailwind4 } from "tailwind-csstree";
 
 export default defineConfigWithVueTs(
-  pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
   {
     ignores: ["dist/**", "node_modules/**", "bun.lockb"],
@@ -50,6 +49,23 @@ export default defineConfigWithVueTs(
       "css/no-invalid-properties": "error",
       "css/no-duplicate-imports": "error",
       "css/use-baseline": "error",
+    },
+  },
+  {
+    files: ["**/*.pcss"],
+    plugins: {
+      css,
+    },
+    language: "css/css",
+    languageOptions: {
+      tolerant: true,
+      customSyntax: tailwind4,
+    },
+    rules: {
+      "css/no-empty-blocks": "error",
+      "css/no-invalid-at-rules": "off",
+      "css/no-invalid-properties": "off",
+      "css/no-duplicate-imports": "error",
     },
   },
   {
