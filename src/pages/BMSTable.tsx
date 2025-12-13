33351ff37f37657b5a0eb275423fd520c02d8ab4
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-12-13 23:05:19 +0800

    chore(vite): inject main.css (#101)

--- src/pages/BMSTable.tsx
+++ src/pages/BMSTable.tsx
@@ -3,7 +3,6 @@ import StarryBackground from "../components/StarryBackground";
 import ChartsTableSection from "./BMSTable/ChartsTableSection";
 import LevelRefTable from "./BMSTable/LevelRefTable";
 import type { ChartData, DifficultyGroup } from "./BMSTable/ChartsTableSection";
-import "../styles/main.css";
 import QuickActions from "../components/QuickActions";
 import "./BMSTable.css";
 
