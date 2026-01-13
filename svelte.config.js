e649ef76274f6e057cf424f485f2f237582250b3
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-01-13 19:38:57 +0800

    fix: check

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import process from "node:process";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
