9491f1f6ed3244ed4b9935dc0c81ee9472e245ae
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-01-16 16:51:56 +0800

    Revert "fix(bmstable): use rel path (#411)"
    This reverts commit fd9d71c8747797a1c7d841be0d948ba3457cbf96.

--- src/routes/bms/table/self-sp/+page.svelte
+++ src/routes/bms/table/self-sp/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { page } from "$app/stores";
   import ChartsTableSection from "$lib/components/bms/ChartsTableSection.svelte";
   import LevelRefTable from "$lib/components/bms/LevelRefTable.svelte";
   import StarryBackground from "$lib/components/StarryBackground.svelte";
@@ -272,7 +273,7 @@
 </script>
 
 <svelte:head>
-  <meta name="bmstable" content="./header.json" />
+  <meta name="bmstable" content={$page.url.origin + "/bms/table/self-sp/header.json"} />
 </svelte:head>
 
 <StarryBackground />
