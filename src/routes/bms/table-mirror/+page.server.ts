c898fef3636aa5c8aab580f5e29e3b61b00d27e4
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-03-10 08:08:52 +0800

    fix: issues, x2

--- src/routes/bms/table-mirror/+page.server.ts
+++ src/routes/bms/table-mirror/+page.server.ts
@@ -3,6 +3,5 @@ import { redirect } from "@sveltejs/kit";
 export const prerender = false;
 
 export const load = () => {
-  // eslint-disable-next-line @typescript-eslint/only-throw-error
-  throw redirect(301, "/bms/table/mirror-proxy");
+  redirect(301, "/bms/table/mirror-proxy");
 };
--- src/routes/bms/table-mirror/[dir_name]/+page.server.ts
+++ src/routes/bms/table-mirror/[dir_name]/+page.server.ts
@@ -1,8 +1,9 @@
 import { redirect } from "@sveltejs/kit";
 
+import type { PageServerLoad } from "./$types";
+
 export const prerender = false;
 
-export const load = ({ params }: { params: { dir_name: string } }) => {
-  // eslint-disable-next-line @typescript-eslint/only-throw-error
-  throw redirect(301, `/bms/table/mirror-proxy/${params.dir_name}`);
+export const load: PageServerLoad = ({ params }) => {
+  redirect(301, `/bms/table/mirror-proxy/${params.dir_name}`);
 };
