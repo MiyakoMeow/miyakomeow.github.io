719c694776bf38c2cccdb73896aebc028cf93ef7
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-03-13 16:18:45 +0800

    fix: advices, x2

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,11 +1,16 @@
+import { resolve } from "node:path";
+
 import type { PageServerLoad } from "./$types";
 
 import { BLOG_DIR } from "$lib/constants/blog";
 import { scanBlogDirectory } from "$lib/utils/blog-scanner";
 import { formatTitle } from "$lib/utils/title";
 
+let cachedPosts: ReturnType<typeof scanBlogDirectory> | undefined;
+
 export const load: PageServerLoad = () => {
-  const posts = scanBlogDirectory(BLOG_DIR);
+  cachedPosts ??= scanBlogDirectory(resolve(process.cwd(), BLOG_DIR));
+  const posts = cachedPosts;
 
   return {
     posts,
